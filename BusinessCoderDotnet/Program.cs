7b102db0ecf71acba86283a988918f37b42a6a32
Melissa <ha@melissadata.com>
2023-10-27 11:43:46 -0700

    Update conditional statements in the main program

--- BusinessCoderDotnet/Program.cs
+++ BusinessCoderDotnet/Program.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Security.Cryptography;
 
@@ -96,11 +96,11 @@ namespace BusinessCoderDotnet
       string APICall = Path.Combine(baseServiceUrl, requestQuery);
       for (int i = 0; i < APICall.Length; i += 70)
       {
-        try
+        if (i + 70 < APICall.Length)
         {
           Console.WriteLine(APICall.Substring(i, 70));
         }
-        catch
+        else
         {
           Console.WriteLine(APICall.Substring(i, APICall.Length - i));
         }
